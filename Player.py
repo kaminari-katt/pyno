c8b29bbc85479289768bdd51e03862ff47d4d1c4
Surendhran-P <119148273+Surendhran-P@users.noreply.github.com>
2023-04-14 19:41:15 +0530

    Player code
    Creation of player.py

---
+++ Player.py
@@ -0,0 +1,14 @@
+# Code for both human and bot players
+
+class Player:
+  def __init__(self,id,nature,hand):
+    self.id = id     # Var for identifying player
+    self.nature = nature # =true for human, =false for bot
+    self.hand = hand #List of cards...To be broadcast to and from distributor
+
+  def playCard(indexOfCard):
+    card = self.hand[indexOfCard] #The card (string) played
+    self.hand.remove(card)
+    
+  def displayCard():
+    print(self.hand) #Can be modified to a func if gui is added

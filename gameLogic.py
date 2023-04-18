92632955f897597f5c266b2fc013e87a424adeb5
Surendhran-P <119148273+Surendhran-P@users.noreply.github.com>
2023-04-18 22:32:19 +0530

    Code layout modification
    After the recent integration of files...the program was throwing errors due to declaration of variables at improper places... Modifications to code layout

--- gameLogic.py
+++ gameLogic.py
@@ -1,4 +1,42 @@
-# Code for both human and bot players
+
+# main Game Logic
+
+import random
+
+numberOfHumanPlayers = 4 #The value just for testing purpose
+
+# cards in each players hand
+playerOneHand = []
+playerTwoHand = []
+playerThreeHand = []
+playerFourHand = []
+# list for code comfortability in looping
+players = [playerOneHand, playerTwoHand, playerThreeHand, playerFourHand]
+
+
+# --- DATA ---
+zeroes = 4
+ones = 8
+twos = 8
+threes = 8
+fours = 8
+fives = 8
+sixes = 8
+sevens = 8
+eights = 8
+nines = 8
+skips = 8
+reverses = 8
+plusTwos = 8
+plusFours = 4 # no colors
+wildCards = 4 # no colors
+# --- DATA ---
+
+# card code tokens
+cardTypes = ["0", "1", "1", "2", "2", "3","3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Skip", "Skip", "Reverse", "Reverse", "+2", "+2", "+4", "W"]
+colors = ["R", "Y", "B", "G"]
+
+deckCards = [] # the cards in the deck
 
 #Characteristics of a playable character
 
@@ -28,44 +66,6 @@ listOfPlayers = [Player(i,True,players[i]) for i in range(numberOfHumanPlayers)]
 #--------------–-------------------------
 
 
-# main Game Logic
-
-import random
-
-# --- DATA ---
-zeroes = 4
-ones = 8
-twos = 8
-threes = 8
-fours = 8
-fives = 8
-sixes = 8
-sevens = 8
-eights = 8
-nines = 8
-skips = 8
-reverses = 8
-plusTwos = 8
-plusFours = 4 # no colors
-wildCards = 4 # no colors
-# --- DATA ---
-
-# card code tokens
-cardTypes = ["0", "1", "1", "2", "2", "3","3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "Skip", "Skip", "Reverse", "Reverse", "+2", "+2", "+4", "W"]
-colors = ["R", "Y", "B", "G"]
-
-numberOfHumanPlayers = 4 #The value just for testing purpose
-
-# cards in each players hand
-playerOneHand = []
-playerTwoHand = []
-playerThreeHand = []
-playerFourHand = []
-# list for code comfortability in looping
-players = [playerOneHand, playerTwoHand, playerThreeHand, playerFourHand]
-
-deckCards = [] # the cards in the deck
-
 # shuffle the cards and place them in deck
 def gameShuffle():
     for cardType in cardTypes:
@@ -77,8 +77,8 @@ def gameShuffle():
 def distribute():
     for i in range(0,7):
         for player in players:
-            player.append(cards[0])
-            deckCards.remove(cards[0]
+            player.append(deckCards[0])
+            deckCards.remove(deckCards[0])
 
 # method to translate card codes into strings that is readable
 def translate(cardCode):
@@ -124,7 +124,7 @@ def translate(cardCode):
     cardName = str(str(color) + str(cardValue) + " Card")
     return cardName
 
-def penalty(self,_player,penaltyType): # To be called when some player needs to draw a card
+def penalty(_player,penaltyType): # To be called when some player needs to draw a card
   if penaltyType == "+2":
     _player.drawCard(2)
   
@@ -133,6 +133,22 @@ def penalty(self,_player,penaltyType): # To be called when some player needs to 
 
 gameShuffle()
 distribute()
+
+# dummy gameLoop
+while True:
+  print('You are player1.....')
+  print('This is your hand of cards ',listOfPlayers[0].displayHand())
+  
+  print('1: play a card, 2: draw a card')
+  user = int(input())
+  
+  if user == 1:
+    print('Which card?')
+    d = int(input())
+    print('Card played: ',listOfPlayers[0].playCard(d-1))
+    
+  if user == 2:
+    print('You drew card')
 
 #Which card is player playing? = method playCard()
 #List of individual players? = listOfPlayers -> a list
